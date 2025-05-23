419a472aa2c1e836acad3b700b0a7bac073dd7b9
jasmi <jasmin_jat@yahoo.de>
2025-05-24 00:40:16 +0200

    init project friendshipbook

---
+++ friendshipbook/src/main/java/com/friendshipbook/friendshipbook/FriendshipbookApplication.java
@@ -0,0 +1,13 @@
+package com.friendshipbook.friendshipbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendshipbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendshipbookApplication.class, args);
+	}
+
+}
---
+++ friendshipbook/src/test/java/com/friendshipbook/friendshipbook/FriendshipbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.friendshipbook.friendshipbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendshipbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
